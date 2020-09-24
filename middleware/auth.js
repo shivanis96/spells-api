a07b39633876fde83127bbbd6018aededc373796
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-09-24 17:20:31 +0100

    add verifyUser funtion to be tested later

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+const model = require("../model/users");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const SECRET = process.env.JWT_SECRET;
+
+const verifyUser = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    const error = new Error("Authorization header required");
+    error.status = 400;
+    next(error);
+  }
+  const token = authHeader.replace("Bearer ", "");
+  try {
+    const data = jwt.verify(token, SECRET);
+    model
+      .getUserById(data.user)
+      .then((user) => {
+        req.user = user;
+        next();
+      })
+      .catch(next);
+  } catch (_error) {
+    const error = new Error("Invalid token");
+    error.status = 401;
+    next(error);
+  }
+};
+
+module.exports = verifyUser;
